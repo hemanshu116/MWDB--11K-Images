9a1a20f79e6a4f6058518ce6eeb0f49ae182302f
Hemanshu Solanki <hemanshu116@gmail.com>
2019-10-08 05:09:58 -0700

    add folder paths

--- Main/config.py
+++ Main/config.py
@@ -1,4 +1,9 @@
 
 IMAGE_PATH = "D:\ASU Projects\CSE 551 - MWDB\Phase-1\CSE 515 Fall19 - Smaller Dataset\Hand_0008110.jpg"
 
-IMAGE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\Phase-1\CSE 515 Fall19 - Smaller Dataset"
+IMAGE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\images"
+
+DATABASE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\Database"
+
+frTechniqueDict = {"1": "CM", "2": "LBP", "3": "HOG", "4": "SIFT"}
+fdTechniqueDict = {"1": "PCA", "2": "LDA", "3": "SVD", "4": "NMF"}
