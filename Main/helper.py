729b9bfdba2642de6760f3f8d7bed78c708107df
Hemanshu Solanki <hemanshu116@gmail.com>
2019-10-08 05:10:46 -0700

    Refactor code and add progress bar and distance function

--- Main/helper.py
+++ Main/helper.py
@@ -1,24 +1,14 @@
-from Main.featureDescriptors.CM import CM
-from Main.featureDescriptors.HOG import HOG
-from Main.featureDescriptors.LBP import LBP
 import numpy as np
 
-from Main.reducers.PCA_Reducer import PCA_Reducer
+
+def progress(count, total):
+    print('\r', str((count / total) * 100) + "% completed", end=' ')
 
 
-def getUserInput():
-    print("Please select the feature descriptor technique")
-    print("1. Color Moments")
-    print("2. Linear Binary Patterns")
-    print("3. Histogram of Oriented Gradients")
-    print("4. SIFT")
-    fdInput = input()
-    print("Please select the feature reduction technique")
-    print("1. PCA")
-    print("2. LDA")
-    print("3. SVD")
-    print("4. NMF")
-    frInput = input()
-    print("Please enter K number of latent semantics")
-    k = input()
-    return [fdInput, frInput, k]
+def findDistance(selectedImage, latentFeatureDict):
+    output = {}
+    for key, value in latentFeatureDict.items():
+        imageFromDatabase = np.asarray(latentFeatureDict[key])
+        output[key] = np.linalg.norm(selectedImage - imageFromDatabase)
+    return output
+
