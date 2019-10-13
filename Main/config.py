72d1657ec7a5f394243d8072714870d2100b6362
destiny4 <magesh22111995@gmail.com>
2019-10-13 16:00:56 -0700

    added svd and started with task3

--- Main/config.py
+++ Main/config.py
@@ -1,9 +1,10 @@
+IMAGE_PATH = "H:\Asu\mwdb\project-phase-0\CSE 515 Fall19 - Smaller Dataset\Phase-1\CSE 515 Fall19 - Smaller Dataset\Hand_0008110.jpg"
 
-IMAGE_PATH = "D:\ASU Projects\CSE 551 - MWDB\Phase-1\CSE 515 Fall19 - Smaller Dataset\Hand_0008110.jpg"
+IMAGE_FOLDER = "H:\Asu\mwdb\project-phase-0\CSE 515 Fall19 - Smaller Dataset\images"
 
-IMAGE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\images"
+DATABASE_FOLDER = "H:\Asu\mwdb\project-phase-0\CSE 515 Fall19 - Smaller Dataset\Database\\"
 
-DATABASE_FOLDER = "D:\ASU Projects\CSE 551 - MWDB\MWDB--11K-Images\Database"
+METADATA_FOLDER = "H:\Asu\mwdb\project-phase-0\CSE 515 Fall19 - Smaller Dataset\metadata"
 
 frTechniqueDict = {"1": "CM", "2": "LBP", "3": "HOG", "4": "SIFT"}
 fdTechniqueDict = {"1": "PCA", "2": "LDA", "3": "SVD", "4": "NMF"}
--- Main/tasks/Task3.py
+++ Main/tasks/Task3.py
@@ -1,2 +1,78 @@
-def startTask3():
-    pass
+import pandas as pd
+
+import Main.config as config
+
+def startTask3(inputs=[], shouldGetInputs=True):
+    if shouldGetInputs:
+        inputs = getUserInputForTask1()
+    fdTechnique = inputs[0]
+    flTechnique = inputs[1]
+    frTechnique = inputs[2]
+    k = inputs[3]
+    meta_data = pd.read_csv(config.METADATA_FOLDER)
+    aspectOfHand = ''
+    accessories = ''
+    gender = ''
+    if flTechnique[1] == '1':
+        aspectOfHand += 'dorsal'
+    elif flTechnique[1] == '2':
+        aspectOfHand += 'palmer'
+    else:
+        print("Wrong input")
+        exit()
+    if flTechnique[0] == '1':
+        aspectOfHand += 'left'
+    elif flTechnique[0] == '2':
+        aspectOfHand += 'right'
+    else:
+        print("Wrong input")
+        exit()
+    if flTechnique[2] == '1':
+        accessories = 1
+    elif flTechnique[2] == '2':
+        accessories = 0
+    else:
+        print("Wrong input")
+        exit()
+    if flTechnique[3] == '1':
+        gender = 'male'
+    elif flTechnique[3] == '2':
+        gender = 'female'
+    else:
+        print("Wrong input")
+        exit()
+    featureVector = meta_data.loc[(meta_data['aspectOfHand'] == aspectOfHand) & (meta_data['accessories'] == accessories) &
+                                  (meta_data['gender'] == gender)]
+
+
+
+
+def getUserInputForTask1():
+    print("Please select the feature descriptor technique")
+    print("1. Color Moments")
+    print("2. Linear Binary Patterns")
+    print("3. Histogram of Oriented Gradients")
+    print("4. SIFT")
+    fdInput = input()
+    print("Please select one of the labels")
+    print("1. Left-hand")
+    print("2. Right-hand")
+    fl1Input = input()
+    print("1. Dorsal")
+    print("2. Palmer")
+    fl2Input = input()
+    print("1. With accessories")
+    print("2. Without accessories")
+    fl3input = input()
+    print("1. Male")
+    print("2. Female")
+    fl4Input = input()
+    print("Please select the feature reduction technique")
+    print("1. PCA")
+    print("2. LDA")
+    print("3. SVD")
+    print("4. NMF")
+    frInput = input()
+    print("Please enter K number of latent semantics")
+    k = input()
+    return [fdInput, [fl1Input, fl2Input, fl3input, fl4Input], frInput, k]
