1434b529687b2144e035b2f92ab9bf2d2e70055d
varun_sumyag <varun@sumyag.com>
2019-10-17 05:17:23 -0700

    Standardizing PCA with StandardScalar

--- Main/reducers/PCA_Reducer.py
+++ Main/reducers/PCA_Reducer.py
@@ -1,6 +1,7 @@
 import pandas as pd
+import numpy as np
 from sklearn.decomposition import PCA
-
+from sklearn.preprocessing import StandardScaler
 
 class PCA_Reducer:
     def __init__(self, featureDescriptor, k):
@@ -8,8 +9,10 @@ class PCA_Reducer:
         self.k = k
 
     def reduceDimension(self):
+        # Standardizing feature descriptors by making mean 0 and std deviation 1.
+        self.featureDescriptor = StandardScaler().fit_transform(self.featureDescriptor)
         pca = PCA(n_components=self.k)
-        # print(type(self.featureDiscriptor))
+        # print('feat desc mean: ', np.mean(self.featureDescriptor), 'feat desc std dec: ',  np.std(self.featureDescriptor))
         principalComponents = pca.fit_transform(self.featureDescriptor)
         # print(principalComponents)
         principalDf = pd.DataFrame(data=principalComponents)
