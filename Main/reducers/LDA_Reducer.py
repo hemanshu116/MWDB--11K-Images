c1e782979fc6f37ef1fb710266510ac0e32eb686
Hemanshu Solanki <hemanshu116@gmail.com>
2019-10-20 13:38:40 -0700

    Implement LDA

--- Main/reducers/LDA_Reducer.py
+++ Main/reducers/LDA_Reducer.py
@@ -2,14 +2,27 @@ import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
+import pandas as pd
+from sklearn.decomposition import LatentDirichletAllocation
+from sklearn.preprocessing import MinMaxScaler
+
+
 class LDA_Reducer:
     def __init__(self, featureDescriptor, k):
         self.featureDescriptor = featureDescriptor
         self.k = k
+        scaler = MinMaxScaler()
+        scaler.fit(featureDescriptor)
+        self.imageID = None
+        self.model = LatentDirichletAllocation(self.k, random_state=0)
+        W = self.model.fit_transform(scaler.transform(self.featureDescriptor))
+        H = self.model.components_
+        self.featureLatentSemantics = H[:self.k, :].T
+        self.objectLatentSemantics = W[:, :self.k]
 
-    def reduceDimension(self):
-        clf = LinearDiscriminantAnalysis(n_components=self.k)
-        clf.fit_transform(self.featureDescriptor)
-        principalDf = pd.DataFrame(data=clf)
-        print(principalDf)
-        return principalDf
+    def reduceDimension(self, data):
+        reducedDimensions = self.model.transform(data)
+        return pd.DataFrame(data=reducedDimensions)
+
+    def saveImageID(self, imageID):
+        self.imageID = imageID
